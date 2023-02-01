2962dde84c00e8ea2c0abe3097e0acca7e80ddb9
coolanimeguy666 <coolanimeguy666@gmail.com>
2023-01-31 17:35:17 -0700

    Add project files.

---
+++ Lexing/Lexing.cpp
@@ -0,0 +1,49 @@
+// Lexing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+std::string SingleCharacterSep = "=+-/*[]{}().";
+
+std::vector<std::string> CheckChar(std::string const input) {
+    std::string buffer;
+    std::vector<std::string> ret;
+
+    bool early_break = false;
+    for (auto character : input) {
+        early_break = false;
+        for (auto illegal : SingleCharacterSep) {
+            if (character == illegal) {
+                if (buffer.length() != 0) {
+                    ret.push_back(buffer);
+                }
+                buffer = character;
+                ret.push_back(buffer);
+                early_break = true;
+                buffer = "";
+                break;
+            }
+        }
+        if (!early_break) {
+            buffer += character;
+        }
+    }
+    if (buffer.length() != 0) {
+        ret.push_back(buffer);
+    }
+    return ret;
+}
+
+int main()
+{
+    std::vector<std::string> Lexemes;
+    std::string input;
+    std::getline(std::cin, input);
+    Lexemes = CheckChar(input);
+    for (auto x : Lexemes) {
+        std::cout << x << "\n";
+    }
+
+    std::cout << "Hello World!\n";
+}
